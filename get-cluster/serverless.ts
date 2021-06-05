046f22ae51802363dfac2a21bbf7f33989690c67
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2021-06-05 21:08:06 +0900

    Rename

--- get-cluster/serverless.ts
+++ get-cluster/serverless.ts
@@ -3,7 +3,7 @@ import type { AWS } from '@serverless/typescript';
 import hello from '@functions/hello';
 
 const serverlessConfiguration: AWS = {
-  service: 'get-latest-cluster',
+  service: 'get-cluster',
   frameworkVersion: '2',
   custom: {
     webpack: {
